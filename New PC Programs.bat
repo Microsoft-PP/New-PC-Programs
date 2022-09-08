aca1445f163f8b9cc7c0ae5b6b3cdf32c06c8403
Microsoft-PP <90161628+Microsoft-PP@users.noreply.github.com>
2022-09-08 11:42:46 -0700

    3.0

---
+++ New PC Programs.bat
@@ -0,0 +1,29 @@
+curl -SL "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -o SteamSetup.exe
+
+curl -SL "https://www.geeks3d.com/dl/get/10195" -o GPU-Tweak-III-v1531.zip
+
+curl -SL "https://discord.com/api/download?platform=win" -o DiscordSetup.exe
+
+curl -SL "https://download.cpuid.com/hwmonitor/hwmonitor_1.46.exe" -o hwmonitor_1.46.exe
+
+curl -SL "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246808_424b9da4b48848379167015dcc250d8d" -o jre-8u341-windows-x64.exe
+
+curl -SL "https://go.microsoft.com/fwlink/?LinkId=691209" -o MediaCreationTool21H2.exe
+
+curl -SL "https://us.download.nvidia.com/GFE/GFEClient/3.25.1.27/GeForce_Experience_v3.25.1.27.exe" -o GeForce_Experience_v3.25.1.27.exe
+
+curl -SL "https://launcher.mojang.com/download/MinecraftInstaller.msi" -o MinecraftInstaller.msi
+
+curl -SL "https://www.diskgenius.com/dyna_download/?software=DGEngSetup5461432.exe" -o DGEngSetup5461432.exe
+
+curl -SL "https://github.com/pbatard/rufus/releases/download/v3.20/rufus-3.20.exe" -o rufus-3.20.exe
+
+curl -SL "https://osdn.net/frs/redir.php?m=nchc&f=crystaldiskinfo%2F77675%2FCrystalDiskInfo8_17_6.exe" -o CrystalDiskInfo8_17_6.exe
+
+curl -SL "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -o chrome_installer.exe
+
+curl -SL "https://www.passmark.com/downloads/petst.exe" -o petst.exe
+
+curl -SL "https://bits.avcdn.net/productfamily_CCLEANER/insttype_FREE/platform_WIN_PIR/installertype_ONLINE/build_RELEASE/cookie_mmm_ccl_003_999_a6i_m" -o ccsetup603.exe
+
+pause
